52094d9d9d297177049c3e7a1ed920cd7af23d45
Phaneendra Chiruvella <hi@pcx.io>
2013-07-13 17:45:39 +0530

    adds feeder.models.Feed, Article models

--- pathrika/feeder/models.py
+++ pathrika/feeder/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Feed(models.Mode):
+    title = models.CharField(max_length=1024, blank=True)
+    subtitle = models.TextField(blank=True)
+    link = models.URLField()
+    href = models.URLField()
+    updated = models.DateTimeField()
+    processed = models.BooleanField(default=False)
+
+class Article(models.Model):
+    feed = models.ForeignKey(Feed)
+    title = models.CharField(max_length=1024, blank=True)
+    link = models.URLField()
+    href = models.URLField()
+    summary = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    author = models.CharField(max_length=256, blank=True)
+    tags = models.CharField(max_length=256, blank=True)
+    published = models.DateTimeField()
