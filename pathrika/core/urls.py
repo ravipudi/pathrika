8909841506df46bed6503c594dd908c336d82cd0
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-14 11:03:07 +0530

    Add home page to url config

--- pathrika/core/urls.py
+++ pathrika/core/urls.py
@@ -1,9 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-                       url(r'^add-feed/$', 'feeder.views.add_feed', name='add-feed'),
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^add-feed/$',
+        'feeder.views.add_feed',
+        name='add-feed'),
+
+    url('^$',
+        TemplateView.as_view(template_name='index.html'),
+        name='home'),
+)
